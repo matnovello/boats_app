482421303c51a12658f626a63c34f9a2b3367064
Mat <mvello66@gmail.com>
2021-04-13 21:09:41 -0400

    adds show action and links to home and show routes

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -3,4 +3,9 @@ class BoatsController < ApplicationController
     @boats = Boat.all
     render "index.html.erb"
   end
+
+  def show
+    @boat = Boat.find(params[:id])
+    render "show.html.erb"
+  end
 end
